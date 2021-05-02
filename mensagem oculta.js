be1df1488d1806be577571ce7a2bfdc8d253aca3
cristianokituxi <cristianoquichindo2@gmail.com>
2021-05-02 11:00:53 -0300

    primeiro commit

---
+++ mensagem oculta.js
@@ -0,0 +1,49 @@
+const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const inputOriginal = document.getElementById('input-original');
+const cifrador = document.getElementById('cifrador');
+const cifrar = document.getElementById('cifrar')
+const resultado = document.getElementById('resultado');
+const classificacao = document.getElementById('classificacao');
+
+const shifMessage = () => {
+    const wordArray = [...inputOriginal.value.toUpperCase()];
+    printChar(0, wordArray);
+}
+
+const printChar = (currentLetterIndex, wordArray) => {
+    if(wordArray.length === currentLetterIndex) return;
+    inputOriginal.value = inputOriginal.value.substring(1)
+    const spanChar = document.createElement("span");
+    resultado.appendChild(spanChar);
+    animateChar(spanChar)
+        .then( () => {
+            const charSinCodificar = wordArray[currentLetterIndex];
+            spanChar.innerHTML = alfabeto.includes(charSinCodificar) ? 
+                alfabeto[(alfabeto.indexOf(charSinCodificar) + parseInt(classificacao.value)) % alfabeto.length] : 
+                charSinCodificar
+            printChar(currentLetterIndex + 1, wordArray);
+        });
+}
+
+const animateChar = spanChar => {
+    let mudançaDeletras = 0;
+    return new Promise(resolve => {
+        const intervalo = setInterval(() => {
+            spanChar.innerHTML = alfabeto[Math.floor(Math.random() * alfabeto.length)];
+            mudançaDeletras++;
+            if(mudançaDeletras === 3) {
+                clearInterval(intervalo);
+                resolve();
+            }
+        }, 50);
+    });
+}
+
+const submit = e => {
+    e.preventDefault();
+    resultado.innerHTML = '';
+    shifMessage()
+}
+
+cifrador.onsubmit = submit;
+cifrador.onClick= submit;
