874e74e8c317247acef585ffaddcb7ac00b7e532
cristianokituxi <cristianoquichindo2@gmail.com>
2023-02-19 10:37:46 -0400

    fix: add funtions for descriptograth

--- mensagem oculta.js
+++ mensagem oculta.js
@@ -1,4 +1,4 @@
-const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const inputOriginal = document.getElementById('input-original');
 const cifrador = document.getElementById('cifrador');
 const cifrar = document.getElementById('cifrar')
@@ -6,22 +6,101 @@ const resultado = document.getElementById('resultado');
 const classificacao = document.getElementById('classificacao');
 
 const shifMessage = () => {
+    var btn = document.getElementById("select").value;
+    var btn2 = document.getElementById("select2").value;
+
+
     const wordArray = [...inputOriginal.value.toUpperCase()];
-    printChar(0, wordArray);
+    const wordArrays = inputOriginal.value;
+
+    if (btn === "cifra de cesar" && btn2 === "cifrar") {
+        printChar(0, wordArray);
+    }
+    if (btn === "cifra de cesar" && btn2 === "decifrar") {
+        printChardesc(0, wordArray);
+
+    }
+    if (btn === "base 64" && btn2 === "cifrar") {
+        printCharBase(wordArray);
+
+    }
+    if (btn === "base 64" && btn2 === "decifrar") {
+        printCharBaseDesc(wordArray);
+
+    }
+
+}
+const ocultRange = () => {
+    var btn = document.getElementById("select").value;
+    if (btn === "base 64") {
+        var btn = document.getElementById("divClassf").style.visibility = "hidden";
+    } else {
+        var btn = document.getElementById("divClassf").style.visibility = " visible";
+
+    }
+
+
 }
 
+const printCharBase = (wordArray) => {
+    if (wordArray.length) {
+        const spanChar = document.createElement("span");
+        resultado.appendChild(spanChar);
+        animateChar(spanChar)
+            .then(() => {
+                const charSinCodificar = inputOriginal.value;
+                spanChar.innerHTML = btoa(charSinCodificar);
+            });
+
+    }
+
+
+}
+
+const printCharBaseDesc = (wordArray) => {
+    if (wordArray.length) {
+        const spanChar = document.createElement("span");
+        resultado.appendChild(spanChar);
+        animateChar(spanChar)
+            .then(() => {
+                const charSinCodificar = inputOriginal.value;
+                spanChar.innerHTML = atob(charSinCodificar);
+            });
+
+    }
+
+
+}
+
+
 const printChar = (currentLetterIndex, wordArray) => {
-    if(wordArray.length === currentLetterIndex) return;
+    if (wordArray.length === currentLetterIndex)
+        return;
     inputOriginal.value = inputOriginal.value.substring(1)
     const spanChar = document.createElement("span");
     resultado.appendChild(spanChar);
     animateChar(spanChar)
-        .then( () => {
+        .then(() => {
             const charSinCodificar = wordArray[currentLetterIndex];
-            spanChar.innerHTML = alfabeto.includes(charSinCodificar) ? 
-                alfabeto[(alfabeto.indexOf(charSinCodificar) + parseInt(classificacao.value)) % alfabeto.length] : 
+            spanChar.innerHTML = alfabeto.includes(charSinCodificar) ?
+                alfabeto[(alfabeto.indexOf(charSinCodificar) + parseInt(classificacao.value)) % alfabeto.length] :
                 charSinCodificar
             printChar(currentLetterIndex + 1, wordArray);
+        });
+}
+const printChardesc = (currentLetterIndex, wordArray) => {
+    if (wordArray.length === currentLetterIndex)
+        return;
+    inputOriginal.value = inputOriginal.value.substring(1)
+    const spanChar = document.createElement("span");
+    resultado.appendChild(spanChar);
+    animateChar(spanChar)
+        .then(() => {
+            const charSinCodificar = wordArray[currentLetterIndex];
+            spanChar.innerHTML = alfabeto.includes(charSinCodificar) ?
+                alfabeto[(alfabeto.indexOf(charSinCodificar) - parseInt(classificacao.value) + alfabeto.length) % alfabeto.length] :
+                charSinCodificar
+            printChardesc(currentLetterIndex + 1, wordArray);
         });
 }
 
@@ -31,7 +110,7 @@ const animateChar = spanChar => {
         const intervalo = setInterval(() => {
             spanChar.innerHTML = alfabeto[Math.floor(Math.random() * alfabeto.length)];
             mudançaDeletras++;
-            if(mudançaDeletras === 3) {
+            if (mudançaDeletras === 3) {
                 clearInterval(intervalo);
                 resolve();
             }
@@ -46,4 +125,4 @@ const submit = e => {
 }
 
 cifrador.onsubmit = submit;
-cifrador.onClick= submit;
+cifrador.onClick = submit;
